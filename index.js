35c6a604db68f268fcabd1250373a27f6c77db14
Sayem Chowdhury <sayem314@gmail.com>
2019-09-19 15:47:43 +0600

    Prettier

--- index.js
+++ index.js
@@ -1,161 +1,242 @@
-const request = require('./request');
+const request = require("./request");
 
 const defaultAPIParameters = {
-    api_version: '1.0',
-    api_token:   '',
-    input:       3
+  api_version: "1.0",
+  api_token: "",
+  input: 3
 };
 
-const unofficialApiUrl = 'https://www.deezer.com/ajax/gw-light.php';
+const unofficialApiUrl = "https://www.deezer.com/ajax/gw-light.php";
 
 class DeezerApi {
-    constructor(requestFactory) {
-        this.baseParameters = defaultAPIParameters;
-    }
+  constructor(requestFactory) {
+    this.baseParameters = defaultAPIParameters;
+  }
 
-    /**
-     * Get a cid for a unofficial api request.
-     *
-     * @return {Number}
-     */
-    static getApiCid() {
-        return Math.floor(1e9 * Math.random());
-    }
+  /**
+   * Get a cid for a unofficial api request.
+   *
+   * @return {Number}
+   */
+  static getApiCid() {
+    return Math.floor(1e9 * Math.random());
+  }
 
-    setCookie(name, value) {
-        request.setCookie(name, value, unofficialApiUrl);
-    }
+  setCookie(name, value) {
+    request.setCookie(name, value, unofficialApiUrl);
+  }
 
-    setParameter(name, value) {
-        this.baseParameters[name] = value;
-    }
+  setParameter(name, value) {
+    this.baseParameters[name] = value;
+  }
 
-    getQueryParameters(parameters) {
-        return Object.assign(this.baseParameters, parameters)
-    }
+  getQueryParameters(parameters) {
+    return Object.assign(this.baseParameters, parameters);
+  }
 
-    getTrackInfo(songID) {
-        return request.post(unofficialApiUrl, this.getQueryParameters({
-            method: 'deezer.pageTrack',
-            cid:    this.constructor.getApiCid()
-        }), {
-            sng_id: songID
-        }, true, true, true);
-    }
+  getTrackInfo(songID) {
+    return request.post(
+      unofficialApiUrl,
+      this.getQueryParameters({
+        method: "deezer.pageTrack",
+        cid: this.constructor.getApiCid()
+      }),
+      {
+        sng_id: songID
+      },
+      true,
+      true,
+      true
+    );
+  }
 
-    getTrackInfos(...songIDs) {
-        return request.post(unofficialApiUrl, this.getQueryParameters({
-            method: 'song.getListAllData',
-            cid:    this.constructor.getApiCid()
-        }), {
-            SNG_IDS: songIDs.map(value => [value, 0])
-        }, true, true, true);
-    }
+  getTrackInfos(...songIDs) {
+    return request.post(
+      unofficialApiUrl,
+      this.getQueryParameters({
+        method: "song.getListAllData",
+        cid: this.constructor.getApiCid()
+      }),
+      {
+        SNG_IDS: songIDs.map(value => [value, 0])
+      },
+      true,
+      true,
+      true
+    );
+  }
 
-    getUserData() {
-        return request.get(unofficialApiUrl, this.getQueryParameters({
-            method: 'deezer.getUserData',
-            cid:    this.constructor.getApiCid()
-        }), true, false, true);
-    }
+  getUserData() {
+    return request.get(
+      unofficialApiUrl,
+      this.getQueryParameters({
+        method: "deezer.getUserData",
+        cid: this.constructor.getApiCid()
+      }),
+      true,
+      false,
+      true
+    );
+  }
 
-    getPlaylistTracks(playlistID) {
-        return request.post(unofficialApiUrl, this.getQueryParameters({
-            method: 'deezer.pagePlaylist',
-            cid:    this.constructor.getApiCid()
-        }), {
-            playlist_id: playlistID,
-            lang:        'en',
-            nb:          -1,
-            start:       0,
-            tab:         0,
-            tags:        true,
-            header:      true
-        }, true, true, true);
-    }
+  getPlaylistTracks(playlistID) {
+    return request.post(
+      unofficialApiUrl,
+      this.getQueryParameters({
+        method: "deezer.pagePlaylist",
+        cid: this.constructor.getApiCid()
+      }),
+      {
+        playlist_id: playlistID,
+        lang: "en",
+        nb: -1,
+        start: 0,
+        tab: 0,
+        tags: true,
+        header: true
+      },
+      true,
+      true,
+      true
+    );
+  }
 
-    getArtistInfo(artistID) {
-        return request.post(unofficialApiUrl, this.getQueryParameters({
-            method: 'artist.getData',
-            cid:    this.constructor.getApiCid()
-        }), {
-            art_id:         artistID,
-            filter_role_id: [0],
-            lang:           'us',
-            tab:            0,
-            nb:             -1,
-            start:          0
-        }, true, true, true);
-    }
+  getArtistInfo(artistID) {
+    return request.post(
+      unofficialApiUrl,
+      this.getQueryParameters({
+        method: "artist.getData",
+        cid: this.constructor.getApiCid()
+      }),
+      {
+        art_id: artistID,
+        filter_role_id: [0],
+        lang: "us",
+        tab: 0,
+        nb: -1,
+        start: 0
+      },
+      true,
+      true,
+      true
+    );
+  }
 
-    getDiscography(artistID) {
-        return request.post(unofficialApiUrl, this.getQueryParameters({
-            method: 'album.getDiscography',
-            cid:    this.constructor.getApiCid()
-        }), {
-            art_id:         artistID,
-            filter_role_id: [0],
-            lang:           'us',
-            nb:             500,
-            nb_songs:       -1,
-            start:          0
-        }, true, true, true);
-    }
+  getDiscography(artistID) {
+    return request.post(
+      unofficialApiUrl,
+      this.getQueryParameters({
+        method: "album.getDiscography",
+        cid: this.constructor.getApiCid()
+      }),
+      {
+        art_id: artistID,
+        filter_role_id: [0],
+        lang: "us",
+        nb: 500,
+        nb_songs: -1,
+        start: 0
+      },
+      true,
+      true,
+      true
+    );
+  }
 
-    getProfile(profileID) {
-        return request.post(unofficialApiUrl, this.getQueryParameters({
-            method: 'deezer.pageProfile',
-            cid:    this.constructor.getApiCid()
-        }), {
-            user_id: profileID,
-            tab:     'loved',
-            nb:      -1
-        }, true, true, true);
-    }
+  getProfile(profileID) {
+    return request.post(
+      unofficialApiUrl,
+      this.getQueryParameters({
+        method: "deezer.pageProfile",
+        cid: this.constructor.getApiCid()
+      }),
+      {
+        user_id: profileID,
+        tab: "loved",
+        nb: -1
+      },
+      true,
+      true,
+      true
+    );
+  }
 
-    getAlbumTracks(albumID) {
-        return request.post(unofficialApiUrl, this.getQueryParameters({
-            method: 'deezer.pageAlbum',
-            cid:    this.constructor.getApiCid()
-        }), {
-            alb_id: albumID,
-            lang:   'us',
-            tab:    0
-        }, true, true, true);
-    }
+  getAlbumTracks(albumID) {
+    return request.post(
+      unofficialApiUrl,
+      this.getQueryParameters({
+        method: "deezer.pageAlbum",
+        cid: this.constructor.getApiCid()
+      }),
+      {
+        alb_id: albumID,
+        lang: "us",
+        tab: 0
+      },
+      true,
+      true,
+      true
+    );
+  }
 
-    getAlbumTrackInfo(albumID) {
-        return request.post(unofficialApiUrl, this.getQueryParameters({
-            method: 'song.getListByAlbum',
-            cid:    this.constructor.getApiCid()
-        }), {
-            alb_id: albumID,
-            start:  '0',
-            nb:     '500'
-        }, true, true, true);
-    }
+  getAlbumTrackInfo(albumID) {
+    return request.post(
+      unofficialApiUrl,
+      this.getQueryParameters({
+        method: "song.getListByAlbum",
+        cid: this.constructor.getApiCid()
+      }),
+      {
+        alb_id: albumID,
+        start: "0",
+        nb: "500"
+      },
+      true,
+      true,
+      true
+    );
+  }
 
-    searchAlternative(trackInfos) {
-        return request.post(unofficialApiUrl, this.getQueryParameters({
-            method: 'search.music',
-            cid:    this.constructor.getApiCid()
-        }), {
-            QUERY:  'artist:\'' + trackInfos.ART_NAME + '\' track:\'' + trackInfos.SNG_TITLE + '\'',
-            OUTPUT: 'TRACK',
-            NB:     50,
-            FILTER: 0
-        }, true, true, true);
-    }
+  searchAlternative(trackInfos) {
+    return request.post(
+      unofficialApiUrl,
+      this.getQueryParameters({
+        method: "search.music",
+        cid: this.constructor.getApiCid()
+      }),
+      {
+        QUERY:
+          "artist:'" +
+          trackInfos.ART_NAME +
+          "' track:'" +
+          trackInfos.SNG_TITLE +
+          "'",
+        OUTPUT: "TRACK",
+        NB: 50,
+        FILTER: 0
+      },
+      true,
+      true,
+      true
+    );
+  }
 
-    getLyrics(songID) {
-        return request.post(unofficialApiUrl, this.getQueryParameters({
-            method: 'song.getLyrics',
-            cid:    this.constructor.getApiCid()
-        }), {
-            sng_id: songID
-        }, true, true, true);
-    }
-};
+  getLyrics(songID) {
+    return request.post(
+      unofficialApiUrl,
+      this.getQueryParameters({
+        method: "song.getLyrics",
+        cid: this.constructor.getApiCid()
+      }),
+      {
+        sng_id: songID
+      },
+      true,
+      true,
+      true
+    );
+  }
+}
 
 const deezerApi = new DeezerApi();
 module.exports = deezerApi;
--- request.js
+++ request.js
@@ -1,67 +1,68 @@
-const got = require('got');
-const { CookieJar } = require('tough-cookie');
+const got = require("got");
+const { CookieJar } = require("tough-cookie");
 const map = new Map();
 
 const DefaultHeaders = {
-    'user-agent':      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
-    'cache-control':   'max-age=0',
-    'accept-language': 'en-US,en;q=0.9,en-US;q=0.8,en;q=0.7',
-    'accept-charset':  'utf-8,ISO-8859-1;q=0.8,*;q=0.7',
-    'content-type':    'text/plain;charset=UTF-8'
+  "user-agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
+  "cache-control": "max-age=0",
+  "accept-language": "en-US,en;q=0.9,en-US;q=0.8,en;q=0.7",
+  "accept-charset": "utf-8,ISO-8859-1;q=0.8,*;q=0.7",
+  "content-type": "text/plain;charset=UTF-8"
 };
 
 class Request {
-    constructor() {
-        this.cookieJar = new CookieJar();
+  constructor() {
+    this.cookieJar = new CookieJar();
+  }
+
+  setCookie(name, value, url) {
+    this.cookieJar.setCookie(name + "=" + value, url, {}, () => {});
+  }
+
+  post(url, parameters, body, enableRetry, useCache, json) {
+    let options = {
+      cookieJar: this.cookieJar,
+      headers: DefaultHeaders,
+      json: json,
+      query: parameters,
+      body: body
+    };
+
+    if (!enableRetry) {
+      options.retry = 0;
     }
 
-    setCookie(name, value, url) {
-        this.cookieJar.setCookie(name + "=" + value, url, {}, () => {});
+    if (useCache) {
+      options.cache = map;
     }
 
-    post(url, parameters, body, enableRetry, useCache, json) {
-        let options = {
-            cookieJar: this.cookieJar,
-            headers: DefaultHeaders,
-            json: json,
-            query: parameters,
-            body: body
-        };
+    return got(url, options);
+  }
 
-        if (!enableRetry) {
-            options.retry = 0
-        }
+  get(url, parameters, enableRetry, useCache, json) {
+    let options = {
+      cookieJar: this.cookieJar,
+      headers: DefaultHeaders,
+      json: json,
+      query: parameters
+    };
 
-        if (useCache) {
-            options.cache = map
-        }
-
-        return got(url, options);
+    if (!enableRetry) {
+      options.retry = 0;
     }
 
-    get(url, parameters, enableRetry, useCache, json) {
-        let options = {
-            cookieJar: this.cookieJar,
-            headers: DefaultHeaders,
-            json: json,
-            query: parameters
-        };
-
-        if (!enableRetry) {
-            options.retry = 0
-        }
-
-        if (useCache) {
-            options.cache = map
-        }
-
-        return got(url, options);
+    if (useCache) {
+      options.cache = map;
     }
 
-    do(...args) {
-        return got(...args)
-    }
-};
+    return got(url, options);
+  }
+
+  do(...args) {
+    return got(...args);
+  }
+}
 
 const request = new Request();
 module.exports = request;
