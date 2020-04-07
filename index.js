4afe28e51d926fc7a9ab15e97b9807af01e07aac
Sayem Chowdhury <sayem314@gmail.com>
2020-04-07 18:32:41 +0600

    don't init automatically

--- index.js
+++ index.js
@@ -26,11 +26,6 @@ const request = (url, params, data) => {
 class DeezerApi {
   constructor(arl) {
     this.baseParameters = defaultAPIParameters;
-    this.initDeezerApi(
-      arl
-        ? arl
-        : '79437723c0024e17559a2f26b2d5023373506cdadd5e29dceda8c7b0aa03accaa40b4b17e8f8d21743edef40a2a16fca3511b5be4ca15c26c871082543c08893c0dd89f0d415378957e05179b3e7ec2c93625d5a8cf2d51a0d177de1a9833aff'
-    );
   }
 
   setParameter(name, value) {
